e7a5eadb74b7d406b2c1fad2cf524aa579c11d82
Victor-sarti <Sartivictor24@gmail.com>
2024-10-29 22:32:13 -0300

    feat: add text and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
+import ManSection from "./components/ManSection";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return ( 
   <>
   <Navbar/>
+  <ManSection/>
   </> 
   );
 }
---
+++ src/components/ManSection.jsx
@@ -0,0 +1,30 @@
+
+const ManSection = () => {
+    return (
+      
+      <div className=" flex-col mt-28 ml-36">
+            <h1 className=" text-xs text-customOrange ">BEST FITNES IN THE TOWN</h1>
+            <div className="mt-10 ">
+                <h1 className="text-6xl">
+                    <div className="flex space-x-4">
+                        <span className="text-customBlack font-bold text-6xl" style={{ WebkitTextStroke: "1px white" }}>
+                            MAKE
+                        </span>
+                        <span className=" font-bold ">YOUR</span>
+                    </div>
+                    <div className="mt-5 font-bold">BODY SHAPE <p className=" text-xs max-w-lg mt-5">Lorem ipsum dolor sit, 
+                        amet consectetur adipisicing elit. Laborum velit quasi reprehenderit 
+                        eius debitis in nulla autem tenetur excepturi doloremque! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veritatis! 
+                        Lorem ipsum dolor si</p> </div>
+                </h1>
+            </div>
+                <div className="mt-7 ">
+                <a href=" " className="  bg-orange-400 hover:bg-orange-500 border-t-orange-600 py-2 px-3 rounded-md button-scale-on-hover text-white hover:text-stone-950 transition-colors duration-200"> Get Started</a>
+
+                </div>
+
+        </div>
+    )
+}
+
+export default ManSection
