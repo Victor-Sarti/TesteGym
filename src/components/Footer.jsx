d3246030e3e0956fc9e2b72485dbdabb64520152
Victor-sarti <Sartivictor24@gmail.com>
2025-01-02 00:00:51 -0300

    feat: add resposiviness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,32 +4,30 @@ import Logo from "../assets/logo.png"; // Importe seu logo aqui
 
 const Footer = () => {
     return (
+        <footer className="p-6 md:p-10 text-white">
+            {/* Config geral */}
+            <div className="relative flex flex-col lg:flex-row justify-center items-start lg:items-center gap-10">
+                {/* Efeito Brush */}
+                <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-100px] w-60 h-60 rounded-full bg-gradient-to-br from-orange-400 via-orange-300 to-transparent opacity-5 blur-md"></div>
 
-        <footer className="p-10 text-white">
-            {/* Config geral*/}
-            <div className="flex flex-col lg:flex-row justify-center items-start lg:items-center gap-10">
-                {/* Efeito Brush*/}
-            <div class="absolute  left-60  mb-32 w-60 h-60 rounded-full bg-gradient-to-br from-orange-400 via-orange-300 to-transparent opacity-5 blur-md"></div>
-
-
-                {/* Logo*/}
-                <div className="flex flex-col items-center lg:items-start text-center lg:text-left mr-10">
+                {/* Logo */}
+                <div className="flex flex-col items-center lg:items-start text-center lg:text-left mr-10 mb-8 lg:mb-0">
                     <img src={Logo} alt="FitClub Logo" className="w-32 mb-4" />
                     <p className="text-gray-400 text-sm lg:text-base mb-4">
                         Take the first step towards a healthier, stronger <br />
                         you with our unbeatable pricing plans. Let's sweat,<br />
                         achieve, and conquer together!
                     </p>
-                    <div className="flex space-x-4 mt-2">
-                        <a href="#" className="text-customOrange hover:text-customWhite "> <FaFacebookF size={23} /></a>
-                        <a href="#" className="text-customOrange  hover:text-customWhite "> <FaInstagram size={23} /></a>
-                        <a href="#" className="text-customOrange hover:text-customWhite "> <FaTwitter size={23} /> </a>
+                    <div className="flex space-x-4 mt-2 justify-center lg:justify-start">
+                        <a href="#" className="text-customOrange hover:text-customWhite"><FaFacebookF size={23} /></a>
+                        <a href="#" className="text-customOrange hover:text-customWhite"><FaInstagram size={23} /></a>
+                        <a href="#" className="text-customOrange hover:text-customWhite"><FaTwitter size={23} /></a>
                     </div>
                 </div>
 
-                <div className="flex gap-16">
+                <div className="flex flex-wrap gap-16 justify-center lg:justify-start">
                     {/* Coluna Company */}
-                    <div className="text-center lg:text-left">
+                    <div className="text-center lg:text-left mb-8 lg:mb-0">
                         <h3 className="text-lg font-semibold mb-4">Company</h3>
                         <ul className="space-y-2">
                             {resourcesLinks.map((link, index) => (
@@ -43,7 +41,7 @@ const Footer = () => {
                     </div>
 
                     {/* Coluna About Us */}
-                    <div className="text-center lg:text-left">
+                    <div className="text-center lg:text-left mb-8 lg:mb-0">
                         <h3 className="text-lg font-semibold mb-4">About Us</h3>
                         <ul className="space-y-2">
                             {platformLinks.map((link, index) => (
@@ -56,9 +54,8 @@ const Footer = () => {
                         </ul>
                     </div>
 
-                    {/* Coluna Contact */}            
-                    
-                    <div className="text-center lg:text-left">
+                    {/* Coluna Contact */}
+                    <div className="text-center lg:text-left mb-8 lg:mb-0">
                         <h3 className="text-lg font-semibold mb-4">Contact</h3>
                         <ul className="space-y-2">
                             {communityLinks.map((link, index) => (
